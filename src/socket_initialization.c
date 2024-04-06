98db45bfc621c0badcd43ce2c3cf23e5e067c2c6
Ethan Delage <ethandelage04@gmail.com>
2024-04-06 23:45:41 +0200

    Add a timeout for send and recv

--- src/socket_initialization.c
+++ src/socket_initialization.c
@@ -9,11 +9,16 @@
 
 int init_socket() {
     int sock_fd;
+    struct timeval tv;
 
     sock_fd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
     if (sock_fd < 0) {
         dprintf(STDERR_FILENO, "ping: %s\n", strerror(errno));
         return -1;
     }
+    tv.tv_sec = 1;
+    tv.tv_usec = 0;
+    setsockopt(sock_fd, SOL_SOCKET, SO_SNDTIMEO, &tv, sizeof(tv));
+    setsockopt(sock_fd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
     return sock_fd;
 }
